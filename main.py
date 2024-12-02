1301b47142b4465e9268cdd28bb39ab9b1e33ec8
Ayesha1206-a <putrinoviarazzarah123@gmail.com>
2024-12-02 10:17:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ st.sidebar.title("Navigasi")
 st.sidebar.markdown("Pilih halaman dari menu di atas.")
 
 st.title("Selamat Datang di Aplikasi Prediksi Kartu Kredit Palsu!")
-st.write("Gunakan sidebar untuk navigasi antar halaman.")
+st.write("Aplikasi deteksi penipuan (fraud detection) memiliki peran yang sangat penting dalam berbagai industri, terutama di sektor keuangan dan perbankan. Aplikasi ini digunakan untuk mendeteksi transaksi yang mencurigakan, seperti transaksi kartu kredit yang tidak sah atau klaim asuransi palsu, yang dapat merugikan perusahaan dan konsumen. Dengan menggunakan algoritma pembelajaran mesin, aplikasi ini dapat menganalisis pola transaksi untuk membedakan antara aktivitas normal dan yang mencurigakan, mengurangi kerugian finansial akibat penipuan. Di sektor e-commerce, aplikasi ini juga mencegah pembelian yang dilakukan dengan identitas palsu atau metode pembayaran yang curang. Selain itu, deteksi penipuan juga penting dalam mendeteksi pencucian uang, memastikan bahwa transaksi yang terjadi tidak melanggar regulasi atau terlibat dalam aktivitas ilegal.")
