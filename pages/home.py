6298d95e42f25e1821ba2c650c3e3c917d8a18d7
Ayesha1206-a <putrinoviarazzarah123@gmail.com>
2024-12-02 11:16:46 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.title("RINGKASAN")
 st.write("""
     Aplikasi ini dapat memprediksi apakah transaksi kartu kredit merupakan transaksi palsu atau tidak.
-    Silakan pilih halaman untuk mengunggah data, visualisasi, atau melakukan prediksi. 
-    a. mengunggah data digunakan untuk menampilkan data secara keseluruhan
-    b. visualisasi digunakan untuk menampilkan grafik dan juga pengelompokan transaksi 
-    c. prediksi digunakan untuk melakukan deteksi akun yang melakukan transaksi anomali dengan standar apabila sebuah akun "v" melakukan transaksi dengan "-" terlalu banyak dan tidak seimbang dengan income maka akun tersebut melalukan anomali
+    Silakan pilih halaman untuk mengunggah data, visualisasi, atau melakukan prediksi. Mengunggah data digunakan untuk menampilkan data secara keseluruhan. Visualisasi digunakan untuk menampilkan grafik dan juga pengelompokan transaksi. Prediksi digunakan untuk melakukan deteksi akun yang melakukan transaksi anomali dengan standar apabila sebuah akun "v" melakukan transaksi dengan "-" terlalu banyak dan tidak seimbang dengan income maka akun tersebut melalukan anomali
 """)
